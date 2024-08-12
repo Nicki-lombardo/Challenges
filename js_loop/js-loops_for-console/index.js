2accaa89ed664909014aaedcbfa3c5e8e15c7bd9
MONICA <monik.ferrer@gmail.com>
2024-08-12 15:53:20 +0200

    added

---
+++ js_loop/js-loops_for-console/index.js
@@ -0,0 +1,23 @@
+console.clear();
+
+
+// let i = 0;  Inicialización de la variable de contador interna
+// i <= 100; Criterio que debe cumplir la variable del contador
+// i++       Regla como se incremente  -- Después de cada iteración del bucle, i++ incrementa el valor de i en 1. Esto asegura que el bucle avance y eventualmente termine cuando i supere 100.
+
+
+// Resumen del flujo del bucle:
+// Inicio: i se inicializa a 0.
+// Condición: Se verifica si i es menor o igual a 100.
+// Ejecución: Si la condición es verdadera, se ejecuta console.log(i).
+// Incremento: i se incrementa en 1.
+// Repetición: El bucle vuelve a verificar la condición y repite el proceso hasta que i sea mayor que 100.
+// console.log(i);
+
+
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i);
+}
+
+
