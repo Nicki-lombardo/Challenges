759de974e91d64c074ba03ccc712c3b7ffb4ba65
MONICA <monik.ferrer@gmail.com>
2024-08-19 11:30:19 +0200

    Challenge fetchData

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,19 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Error");
+    }
+
+    const data = await response.json();
+    console.log(data);
+    console.log("First person: ", data.results[0]);
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
 
 fetchData();
