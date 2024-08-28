dd42f05f49ff39a3e409295ff61530ad882fae29
MONICA <monik.ferrer@gmail.com>
2024-08-28 11:47:50 +0200

    .map metodo para el array con objeto - add frutas en el array con un id

--- react-with-arrays_fruits/src/App.jsx
+++ react-with-arrays_fruits/src/App.jsx
@@ -1,13 +1,20 @@
 import Card from "./components/Card";
 import "./App.css";
 
-
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: "🍌 Banana", color: "yellow" },
+    { id: 2, name: "🍎 Apple", color: "red" },
+    { id: 3, name: " Grape", color: "purple" },
+    { id: 4, name: " 🥭 Mango", color: "orange" },
+    { id: 5, name: "🍌 Ananas", color: "yellow" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} />
+      ))}
     </div>
   );
 }
